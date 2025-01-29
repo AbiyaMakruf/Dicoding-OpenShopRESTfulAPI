d6d3beec28b4d7f9cb6742fb7d10c3e02552e5ec
Abiya <aabbiiyyaa@gmail.com>
2025-01-29 13:01:41 +0700

    add: menambahkan model Product

--- openShop/models.py
+++ openShop/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=50)
+    shop =  models.CharField(max_length=25)
+    price = models.FloatField()
+    sku = models.CharField(max_length=5)
+    description = models.TextField()
+    location = models.CharField(max_length=50)
+    discount = models.IntegerField()
+    category = models.CharField(max_length=25)
+    stock = models.IntegerField()
+    is_available = models.BooleanField()
+    picture = models.ImageField()
